73d9c714864a4d35bfa5aa96324997cb316e8ec4
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-03-09 18:05:11 +0100

    update

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
 require "vendor/autoload.php";
+require "mail_info.php";
 
 $nginx_status = 'curl http://127.0.0.1/nginx_status';
 
-$output = shell_exec($nginx_status );
+$output = shell_exec($nginx_status);
 
 
 $mail = new Nette\Mail\Message;
-$mail->setFrom('RPI <jonnybaronny@gmail.com>')
-    ->addTo('jonnybaronny@gmail.com')
+$mail->setFrom("RPI <" . $mail["from"] . ">")
+    ->addTo($mail["to"])
     ->setSubject('Nginx status')
     ->setBody("This is your nginx status: " . $output);
 
